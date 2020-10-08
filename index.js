b67f23f3c55c21ba5b8b4b2017600f5d3fe35721
Nico Herrera <nherrera0831@gmail,com>
2020-10-07 20:49:50 -0700

    onto task 5

--- index.js
+++ index.js
@@ -41,8 +41,25 @@ class Airplane {
 */
 
 class Person {
+  constructor (props) {
+this.stomach = [];
+this.age = props.age;
+this.name = props.name;
+  }
+  eat(food) {
+    return this.stomach.push(food);
+    };
+  poop() {
+    return this.stomach.length = 0;
+    }
+  toString() {
+    return `${this.name}, ${this.age}`;
+    }
+  };
 
-}
+  const nico = new Person('Nico', 23);
+  // nico.eat('pizza');
+  // console.log(nico.stomach);
 
 /*
   TASK 2
@@ -59,8 +76,29 @@ class Person {
 */
 
 class Car {
-
+constructor (model, milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0;
+  this.milesPerGallon = milesPerGallon  
 }
+fill(gallons) {
+  return this.tank = this.tank + gallons;
+}
+drive(distance) {
+  this.odometer = this.odometer + distance;
+  this.tank = this.tank - (distance / this.milesPerGallon)
+  if (this.tank <= 0) {
+    console.log(`I ran out of fuel at ${this.odometer} miles!`);
+  };
+  };
+};
+
+const car1 = new Car('tlx', 20);
+
+car1.fill(2);
+car1.drive(35);
+
+// console.log(car1);
 
 /*
   TASK 3
@@ -75,7 +113,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+constructor (obj) {
+  this.name = obj.name;
+  this.age = obj.age;
+  this.location = obj.location;
+}
+speak() {
+  return `Hello my name is ${this.name}, I am from ${this.location}.`
+}
 }
 
 /*
@@ -92,8 +137,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+constructor (obj) {
+  super (obj);
+  this.specialty = 'coding';
+  this.favLanguage = 'JavaScript';
+  this.catchPhrase = 'Down for my homies';
+}
+demo (subject) {
+return `Today we are learning about ${subject}.`
+}
+grade (student, subject) {
+return `${student} receives a perfect score on ${subject}`
+}
 }
 
 /*
